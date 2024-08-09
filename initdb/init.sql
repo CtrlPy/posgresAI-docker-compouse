347b41a060f546a7e67561c33c6945cecd82a0bb
ctrlpy <ctrlpy3@gmail.com>
2024-08-09 20:49:47 +0300

    fix: change volumes, change server config logicalRestore

---
+++ initdb/init.sql
@@ -0,0 +1,10 @@
+-- initdb/init.sql
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100) UNIQUE NOT NULL
+);
+
+INSERT INTO users (name, email) VALUES
+('Alice', 'alice@example.com'),
+('Bob', 'bob@example.com');
